458cfaf4da8101d2d270c3807399831aa424b304
Aldo Peralta <aldo.peralta09@gmail.com>
2019-02-22 15:22:01 -0600

    Schedule CRUD

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,29 @@ router.get('/', async (req, res) => {
 router.get('/detail/:id', async (req, res) => {
   const evento = await Event.findById(req.params.id);
   const schedule = await Schedule.find({name: evento.dates});
-  res.render('detail',{evento});
+  let horario = "";
+  if (schedule.monday != "") {
+    horario = horario + '<p class="card-text"> Lunes: ' + schedule.monday + '</p>';
+    if (schedule.tuesday != "") {
+      horario = horario + '<p class="card-text"> Martes: ' + schedule.tuesday + '</p>';
+      if (schedule.wednesday != "") {
+        horario = horario + '<p class="card-text"> Miercoles: ' + schedule.wednesday + '</p>';
+        if (schedule.thursday != "") {
+          horario = horario + '<p class="card-text"> Jueves: ' + schedule.thursday + '</p>';
+          if (schedule.friday != "") {
+            horario = horario + '<p class="card-text"> Viernes: ' + schedule.friday + '</p>';
+            if (schedule.saturday != "") {
+              horario = horario + '<p class="card-text"> Sabado: ' + schedule.saturday + '</p>';
+              if (schedule.sunday != "") {
+                horario = horario + '<p class="card-text"> Domingo: ' + schedule.sunday + '</p>';
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  res.render('detail',{evento,horario});
 });
 
 module.exports = router;
